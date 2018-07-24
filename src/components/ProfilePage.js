682da28d0f625bf686a34a7a7d7af5710ff07209
NirajFonseka <fonseka.live@utexas.edu>
2018-07-23 21:04:36 -0500

    Changed the grid abit

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -21,8 +21,8 @@ class ProfilePage extends Component {
       return (
        <div>
         <Grid container spacing={24} style={{padding: 24}}>
-            <Grid item xs={12} sm={6} lg={4} xl={3}>
-            <Card raised>
+            <Grid item lg={4} xl={3}>
+             <Card raised>
                     <CardMedia style={{height: 200 , paddingTop : '56.25%'}}
                         image={"https://s3-us-west-2.amazonaws.com/nirajfonsekacom/16969962.jpeg"}
                         title="Photo"
@@ -40,46 +40,53 @@ class ProfilePage extends Component {
                 </Card>
                 
             </Grid>
+
             <Grid item xs={12} sm={6} lg={4} xl={3}>
-            <Card raised style={{maxHeight:300 , maxWidth : 300 }}>
+             <Card raised>
+                    <CardMedia style={{height: 200 , paddingTop : '56.25%'}}
+                        image={"https://s3-us-west-2.amazonaws.com/nirajfonsekacom/16969962.jpeg"}
+                        title="Photo"
+                    />
+                    <CardContent>
+                        <Typography component="p">
+                            The profile description goes here
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" color="primary" href="http://google.com" target="_blank">
+                            Go To Course
+                        </Button>
+                    </CardActions>
+                </Card>
+                
+            </Grid>
+        </Grid>
+        
+            {/* <Grid item xs={12} sm={6} lg={4} xl={3}>
+            <Card raised style={{minHeight:300 }}>
                     <CardMedia style={{ maxHeight:300 , maxWidth: 300, paddingTop : '75.25%'}}
                         image={"https://s3-us-west-2.amazonaws.com/nirajfonsekacom/UT300x300.jpeg"}
                         title="Photo"
                     />
-                    {/* <CardContent>
+                    <CardContent>
                      
                     </CardContent>
                     <CardActions>
                         <Button size="small" color="primary" href="http://google.com" target="_blank">
                             Go To Course
                         </Button>
-                    </CardActions> */}
+                    </CardActions>
             </Card>
-            <Card raised style={{maxHeight:300 , maxWidth : 300 }}>
-                    <CardMedia style={{ maxHeight:300 , maxWidth: 300, paddingTop : '75.25%'}}
-                        image={"https://s3-us-west-2.amazonaws.com/nirajfonsekacom/UT300x300.jpeg"}
-                        title="Photo"
-                    />
-                    {/* <CardContent>
-                     
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small" color="primary" href="http://google.com" target="_blank">
-                            Go To Course
-                        </Button>
-                    </CardActions> */}
-            </Card>
-            {/* <Paper elevation={1}>
+            <Paper elevation={1}>
                 <Typography variant="headline" component="h3">
                     This is a sheet of paper.
                 </Typography>
                 <Typography component="p">
                      Paper can be used to build surface or other elements for your application.
                 </Typography>
-            </Paper> */}
+            </Paper>
                 
-            </Grid>
-        </Grid>
+            </Grid> */}
         </div>
       );
     }
