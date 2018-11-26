bf471d7a1444c8dacae5a3f94c9af2c1ab554d2e
Niraj-Fonseka <fonseka.live@utexas.edu>
2018-11-25 22:43:28 -0600

    Seeting cicd again

---
+++ scripts/deploy-prod.sh
@@ -0,0 +1,3 @@
+ #!/usr/bin/env bash
+
+echo "--- Production Deployment Initiated ---"
---
+++ scripts/deploy-stage.sh
@@ -0,0 +1,8 @@
+ #!/usr/bin/env bash
+
+echo "--- Stage Deployment Initiated ---"
+
+
+npm run build
+
+npm deploystage
