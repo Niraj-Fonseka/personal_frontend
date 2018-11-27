3ddad1db2bdc1355868588a9d03d4dbfb715f360
Niraj-Fonseka <fonseka.live@utexas.edu>
2018-11-27 00:17:07 -0600

    Adding the social media bar

--- src/mobile_app/MainPage.js
+++ src/mobile_app/MainPage.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Bounce from 'react-reveal/Bounce';
+import Fade from 'react-reveal/Fade';
+import SocialMedia from './SocialMedia';
 import Typography from '@material-ui/core/Typography';
 
 
@@ -14,16 +17,23 @@ class MainPage extends Component {
     render(){
           return (
                 <div className="mobile-page">
-                    <Typography noWrap='false' style={{whiteSpace: 'normal',display: 'flex', fontSize: '25px', marginTop: '5px', justifyContent: 'center'}}>
-                        Welcome to my website ! 
-                    </Typography>
-                    <div className="mobile-prof-pic">
-                         <img src='https://s3-us-west-2.amazonaws.com/nirajfonsekacom/16969962.jpeg' style={{ maxHeight: '50%', maxWidth: '50%'}} />
-                    </div>
+                    <Fade top>
+                        <Typography noWrap='false' style={{whiteSpace: 'normal',display: 'flex', fontSize: '25px', marginTop: '10px',marginBottom: '10px', justifyContent: 'center'}}>
+                            Welcome to my website ! 
+                        </Typography>
+                    </Fade>
+                    <Bounce top>
+                        <div className="mobile-prof-pic">
+                            <img src='https://s3-us-west-2.amazonaws.com/nirajfonsekacom/16969962.jpeg' style={{ maxHeight: '50%', maxWidth: '50%'}} />
+                        </div>
+                    </Bounce>
+
                     <Typography noWrap='false' style={{whiteSpace: 'normal', margin: '10px', color:'#fff'}}>
                     Hello! My Name is Niraj Fonseka. I graduated from the University of Texas with a degree in 
                     Computer Science and a Minor in Business. I'm currently working as a Software Engineer at The Home Depot. 
                     </Typography>
+
+                    <SocialMedia />
                 </div>
             )
         }
---
+++ src/mobile_app/SocialMedia.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './../App.css';
+import linkedin from '../Images/linkedin64.png'
+import twitter from '../Images/twitter64.png'
+import instagram from '../Images/instagram64.png'
+import github from '../Images/github64.png'
+import Bounce from 'react-reveal/Bounce';
+
+
+class SocialMedia extends Component {
+  render() {
+    return (
+      <div  style={{ display: 'flex', justifyContent: 'center' , marginTop: '40px'}}>
+            
+            <Bounce left>
+                <a href="https://www.linkedin.com/in/niraj-fonseka-03bab3107">
+                    <img src={linkedin}  style={{ marginRight :'10px'}} />
+                </a>
+             </Bounce>
+
+             <Bounce left>
+                <a href="https://twitter.com/NirajFonseka1">
+                    <img src={twitter} style={{ marginRight :'10px'}} />
+                </a>
+             </Bounce>
+
+             <Bounce right>
+                <a href="https://www.instagram.com/nsf1313/">
+                    <img src={instagram} style={{ marginRight :'10px'}}/>
+                </a>
+             </Bounce>
+
+             <Bounce right>
+                <a href="https://github.com/Niraj-Fonseka">
+                    <img src={github} />
+                </a>
+             </Bounce> 
+      </div>
+    );
+  }
+}
+
+export default SocialMedia;
